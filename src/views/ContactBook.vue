c4ee19b8b81bd8c94ba80ba95358070dfcb68f81
minh377 <minhdc21v7n1523@vlvh.ctu.edu.vn>
2025-10-06 22:52:26 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -69,4 +69,15 @@ export default {
     this.refreshList();
   },
 };
+<ContactCard :contact="activeContact" />
+<router-link
+:to="{
+name: 'contact.edit',
+params: { id: activeContact._id },
+}"
+>
+<span class="mt-2 badge badge-warning">
+<i class="fas fa-edit"></i> Hiệu chỉnh</span
+>
+</router-link>
 </script>
